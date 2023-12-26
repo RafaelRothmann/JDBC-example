596178b14f26e93a3b298ace79e08ab2aaa4a949
RafaelRothmann <rafinharothmann@gmail.com>
2023-12-26 18:10:59 -0300

    Update in database

--- sql/script.sql
+++ sql/script.sql
@@ -10,11 +10,18 @@ create table Departament (
 create table Employee (
     id int Primary Key,
     nome varchar(50) NOT NULL,
+    salario float,
     id_departamento int,
-    FOREIGN key (id_departamento) references Departament(id)
+    FOREIGN key (id_departamento) references Departament(id),
+    Check (salario > 0 and salario is NOT NULL)
 );
 
 insert into Departament (id,nome) value (1,"RH");
 insert into Departament (id,nome) value (2,"DESING");
 insert into Departament (id,nome) value (3,"DESENVOLVEDOR");
 insert into Departament (id,nome) value (4,"GERENCIA");
+
+insert into Employee (id,nome,salario,id_departamento) value (1,"Daniel",1000,1);
+insert into Employee (id,nome,salario,id_departamento) value (2,"Bob",1500,2);
+insert into Employee (id,nome,salario,id_departamento) value (3,"Maria",3000,3);
+insert into Employee (id,nome,salario,id_departamento) value (4,"Rafael",10000,4);
--- src/App.java
+++ src/App.java
@@ -1,34 +1,25 @@
 
-import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import db.DB;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
         Connection conn = null;
         PreparedStatement st = null;
-        Scanner sc = new Scanner(System.in);
-        ResultSet rs = null;
 
         try {
             conn = DB.getConnection();
             
-            int id = 1;
-            String nome = "Rafael";
-            int id_departamento = 1;
-
             st = conn.prepareStatement(
-                "INSERT INTO Employee (id,nome,id_departamento) VALUES (?,?,?)"
+                "UPDATE Employee SET salario = (salario * ?) WHERE (id = ? and nome = ?)"
             );
 
-            st.setInt(1, id);
-            st.setString(2, nome);
-            st.setInt(3, id_departamento);
+            st.setInt(1, 2);
+            st.setInt(2, 1);
+            st.setString(3, "Daniel");
 
             int rowsAffected = st.executeUpdate();
 
@@ -38,8 +29,6 @@ public class App {
             e.printStackTrace();
         } finally {
 
-            sc.close();
-            DB.closeResultsSet(rs);
             DB.closeStatement(st);
             DB.closeConnection();
         }
